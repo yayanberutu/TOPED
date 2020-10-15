fac246f67168449c90cf18fa38d08ee4cb053534
Yosepri Disyandro Berutu <yoseprib@gmail.com>
2020-10-15 15:05:08 +0700

    [YOS] Fix increment seen

--- src/main/java/del/ac/id/demo/controller/ItemController.java
+++ src/main/java/del/ac/id/demo/controller/ItemController.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +22,7 @@ import del.ac.id.demo.repository.ItemRepository;
 @RestController
 public class ItemController {
 	@Autowired ItemRepository itemRepository;
+	@Autowired MongoTemplate mongoTemplate;
 	
 	@RequestMapping("/item")
 	public ModelAndView item() {
@@ -35,29 +39,12 @@ public class ItemController {
 	
 	@GetMapping("/update/{id}")
 	public RedirectView update(@PathVariable (value="id") String id) {
-		Optional<Item> item = itemRepository.findById(id);
-//		List<Item> lsItem = itemRepository.findAll();
-		
-		Item it = item.get();
-		it.setSeen(it.getSeen()+1);
-		MongoTemplate mtemp = new MongoTemplate();
-		mtemp.save(it);
-		
-
-//		System.out.println(it.getItem_name());
-//		if(lsItem.contains(it)) {
-//			System.out.print("Yes");			
-//		}
-//		else {
-//			System.out.println(lsItem.size());
-//			System.out.print("No");
-//		}
-
-//		System.out.println(lsItem.indexOf(it));
-//		lsItem.get(lsItem.indexOf(it)-1).setSeen(it.getSeen()+1);;
-//		itemRepository.deleteAll();
-//		itemRepository.saveAll(lsItem);
-
+		Query query = new Query(Criteria.where("id").is(id));
+		Item item = mongoTemplate.findOne(query, Item.class);
+		if(item != null) {
+			Update update = new Update().inc("seen", 1);
+			mongoTemplate.updateFirst(query, update, Item.class);
+		}
 
 		return new RedirectView("/item");
 	}
